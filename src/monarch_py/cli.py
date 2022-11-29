ecaa63a09738bf505ee9cab531b2d75591abfe47
Kevin Schaper <kevinschaper@gmail.com>
2022-11-28 23:48:23 -0800

    formatting

--- src/monarch_py/cli.py
+++ src/monarch_py/cli.py
@@ -1,6 +1,7 @@
-import typer
-import pathlib
 import importlib
+import pathlib
+
+import typer
 
 from monarch_py.implementations.solr.solr_implentation import SolrImplementation
 
@@ -13,9 +14,11 @@ def schema():
     This method prints the linkml schema to stdout, so that downstream tools can extend the schema as necessary
     """
     schema_name = "model"
-    schema_dir = pathlib.Path(importlib.util.find_spec(f"monarch_py.datamodels.{schema_name}").origin).parent
+    schema_dir = pathlib.Path(
+        importlib.util.find_spec(f"monarch_py.datamodels.{schema_name}").origin
+    ).parent
     schema_path = schema_dir / pathlib.Path(schema_name + ".yaml")
-    with open(schema_path, 'r') as schema_file:
+    with open(schema_path, "r") as schema_file:
         print(schema_file.read())
 
 
--- tests/integration/solr/test_entity.py
+++ tests/integration/solr/test_entity.py
@@ -1,5 +1,6 @@
 from monarch_py.implementations.solr.solr_implentation import SolrImplementation
 
+
 def test_entity():
     si = SolrImplementation()
     entity = si.get_entity("MONDO:0007947")
