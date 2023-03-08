c791dda190d93be145d7c290cfc6cfa5d452a714
glass <glass@glass-ships.com>
2023-03-08 12:54:47 -0700

    need to rework solr tests

--- tests/integration/test_solr_entity.py
+++ tests/integration/test_solr_entity.py
@@ -6,3 +6,4 @@ def test_entity():
     entity = si.get_entity("MONDO:0007947")
     assert entity
     assert entity.name == "Marfan syndrome"
+
---
+++ tests/integration/test_solr_histopheno.py
@@ -0,0 +1,12 @@
+from monarch_py.implementations.solr.solr_implementation import SolrImplementation
+
+def test_histopheno():
+    si = SolrImplementation()
+    hp = si.get_histopheno("MONDO:0020121")
+
+    total = 0
+    for k in hp.items:
+        total += k.count
+        
+    assert hp
+    assert total == hp.total
