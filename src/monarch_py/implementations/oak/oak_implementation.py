7b72c45b471f0215ba34dc27c94cbeacc79d5349
glass-ships <glass.ships@outlook.com>
2023-06-28 10:30:09 -0600

    started stubbing out linkml model for semsim

--- src/monarch_py/implementations/oak/oak_implementation.py
+++ src/monarch_py/implementations/oak/oak_implementation.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 # from typing import Iterable, List
 
 from oaklib.interfaces.semsim_interface import SemanticSimilarityInterface
-# from oaklib.datamodels.similarity import TermSetPairwiseSimilarity
+from oaklib.datamodels.similarity import TermSetPairwiseSimilarity, 
 from oaklib.selector import get_adapter
 
 
@@ -21,5 +21,14 @@ class OakImplementation(SemanticSimilarityInterface):
 
 
     def termset_pairwise_similarity(self, ts1, ts2, predicates=None, labels=False) -> float:
-        sim = self.oi.termset_pairwise_similarity(ts1, ts2, predicates=predicates, labels=labels)
-        return sim
+        tsps = self.oi.termset_pairwise_similarity(ts1, ts2, predicates=predicates, labels=labels)
+
+        average_score = tsps["average_score"]
+        best_score = tsps["best_score"]
+        subject_termset = tsps["subject_termset"]
+        object_termset = tsps["object_termset"]
+        subject_best_matches = tsps["subject_best_matches"]
+        object_best_matches = tsps["object_best_matches"]
+
+
+        return tsps
